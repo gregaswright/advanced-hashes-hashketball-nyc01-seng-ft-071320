bcd9b84b68dab2315b4110d0820f834ee9cc78ad
Gregory A.S. Wright <geslim@gmail.com>
2020-07-06 18:13:39 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -131,7 +131,7 @@ def num_points_scored(player_name)
   game_hash.each do |location, team_data|
     team_data.each do |atributes, data|
       if atributes == :players
-        binding.pry
+        atributes[:players][:points]
 
 
       end
