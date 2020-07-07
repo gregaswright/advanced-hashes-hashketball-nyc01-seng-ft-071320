9c9655d9a2e3f9d44c0873aa23d78fe0ef8f2274
Gregory A.S. Wright <geslim@gmail.com>
2020-07-07 11:00:37 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -149,7 +149,7 @@ end
 
 def team_colors(team_name)
   game_hash.each do |location, team_data|
-    if team_data == team_name
+    if team_data[:team_name] == team_name
       team_data[:colors]
     end
   end
