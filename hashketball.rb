c15f841cfa44c91f9a853c37700ee3b002c112ed
Gregory A.S. Wright <geslim@gmail.com>
2020-07-07 11:31:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -165,8 +165,8 @@ def player_numbers(team_name)
   game_hash.each do |location, team_data|
     if team_data[:team_name] == team_name
     team_data[:players].each do |player|
-      binding.pry
-      return player[:number]
+
+      player[:number]
       end
     end
   end
